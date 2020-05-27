6c501668a05e8c07a764e00740dca77017f3630b
Manoel Tindão <timdao02@hotmail.com>
2020-05-27 13:28:13 -0300

    Criação do projeto

---
+++ src/test/java/com/tindao/ApiRestFul01/ApiRestFul01ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tindao.ApiRestFul01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiRestFul01ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
